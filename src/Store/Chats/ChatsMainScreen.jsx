19b5f5e0be8e4f132fd64c15d54276bf2ba86223
Abhi <soni5abhi23@gmail.com>
2024-10-16 02:01:42 +0530

    Adding chat api

--- src/Store/Chats/ChatsMainScreen.jsx
+++ src/Store/Chats/ChatsMainScreen.jsx
@@ -1,23 +1,38 @@
 import React from 'react'
 import ChatBubble from '../../Components/ChatComponents/ChatBubble'
 import './ChatMainScreen.css'
-import { useState } from 'react'
+import { useState, useContext } from 'react'
 import axios from 'axios'
-
+import DateTime from '../../Components/DateTime'
+import { UserContext } from '../../contexts/Context'
 
 export default function ChatsMainScreen () {
+  const { user } = useContext(UserContext)
   const [text, setText] = useState('')
 
   const chatHandler = e => {
     console.log('Inside chatHandler function', text)
     setText('')
     e.preventDefault()
+    let time = DateTime()
+    console.log('Current time', time)
 
-    const resp = axios.post('http://localhost:3000/signup', {
+    try {
+      const resp = axios.post('http://localhost:3000/chat', {
+        user,
         text,
+        time
+      })
 
+      if (resp) {
+        //   console.log('Chat sent successfully')
+        if (resp.response == 'ok') {
+          console.log('Chat sent successfully')
+        }
+      }
+    } catch (error) {
+      console.log(error)
     }
-    )
   }
 
   return (
