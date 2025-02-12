9cb684ae94045702bbcfa652a37b985db8eac1e9
Abhi <soni5abhi23@gmail.com>
2025-02-12 23:28:21 +0530

    adding changes to navbar

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,25 +1,59 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import './Navbar.css'
 import { UserContext } from '../../contexts/Context'
 import Profile from '../../Store/Profile/Profile'
 import { useNavigate } from 'react-router-dom'
 import Search from '../SearchComp/Search'
+import logo from '../../Assets/logo.png'
 
 function Navbar () {
   const { user, setUser } = useContext(UserContext)
+  const [userSearch, setUserSearch] = useState(false)
   const navigate = useNavigate()
 
   const logoutHandler = () => {
     localStorage.removeItem('token')
     setUser('')
   }
+  const toggleEvent = () => {
+    setUserSearch(prev => !prev) // Toggle the state
+  }
 
   return (
     <div className='Navbar'>
       <ul className='Navbar-ul'>
-        {/* <li>ImageLogo</li> */}
-        <li><Search  /></li>
-        <li onClick={() => navigate('/dashboard/Chats', { replace: true })}>
+        <li>
+          <img src={logo} alt='' width={'80px'} height={'50px'} />
+        </li>
+        <li onClick={toggleEvent}>Search</li>
+
+        <li className='searchBar'>
+          <Search />
+        </li>
+        <li>
+          <div class='dropdown'>
+            <button class='dropbtn'>
+              Dropdown
+              <i class='fa fa-caret-down'></i>
+            </button>
+            <div class='dropdown-content'>
+              <a
+                onClick={() => navigate('/dashboard/Chats', { replace: true })}
+              >
+                Chat
+              </a>
+              <a
+                onClick={() => {
+                  navigate('/dashboard/Profile', { replace: true })
+                }}
+              >
+               Profile
+              </a>
+              <a onClick={logoutHandler}>Logout</a>
+            </div>
+          </div>
+        </li>
+        {/* <li onClick={() => navigate('/dashboard/Chats', { replace: true })}>
           Chat
         </li>
         <li
@@ -29,7 +63,7 @@ function Navbar () {
         >
           Profile
         </li>
-        <li onClick={logoutHandler}>Logout</li>
+        <li onClick={logoutHandler}>Logout</li> */}
       </ul>
     </div>
   )
